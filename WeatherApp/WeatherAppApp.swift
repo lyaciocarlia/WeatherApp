e3f084214854a57e75aa15860e4880bc5e1f6f7b
lyaciocarlia <lyass.media@gmail.com>
2023-08-23 13:58:10 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Iuliana Stecalovici  on 23.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
