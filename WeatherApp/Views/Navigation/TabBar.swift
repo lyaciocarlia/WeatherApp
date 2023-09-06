288de9b7676ff241e966cb1ed0f4f84bab0a1dfe
lyaciocarlia <lyass.media@gmail.com>
2023-09-06 19:51:00 +0300

    Initial Setup

---
+++ WeatherApp/Views/Navigation/TabBar.swift
@@ -0,0 +1,20 @@
+//
+//  TabBar.swift
+//  WeatherApp
+//
+//  Created by Iuliana Stecalovici  on 23.08.2023.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        Text("Hello world")
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}
